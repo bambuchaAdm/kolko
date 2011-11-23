6cc34283f0410372b6facb049e3bf710ee7e6689
Jacek Burys <jacekburys@gmail.com>
2011-11-23 17:16:21 +0100

    abc

--- src/Sort.java
+++ src/Sort.java
@@ -1,19 +1,26 @@
 
 public class Sort {
 	
-	public void zamien(int[]tab, int a, int b)
+	private int[] tab;
+	
+	Sort(int[] tab)
+	{
+		this.tab = tab;
+	}
+	
+	private void zamien(int a, int b)
 	{
 		int temp=tab[a];
 		tab[a]=tab[b];
 		tab[b]=temp;
 	}
 	
-	public void bsort(int[] tab)
+	private void bsort()
 	{
 	for(int i=0; i<tab.length; i++)
 		for(int i2=0; i2<tab.length-1; i2++)
 			if(tab[i]<tab[i2])
-				zamien(tab,i, i2 );
+				zamien(i, i2 );
 		
 	}
 	
@@ -21,8 +28,8 @@ public class Sort {
 	public static void main(String[] args) 
 	{
 	int[] tab ={3, 453, 53, 46, 75, 76, 23, 352};
-	Sort sort=new Sort();
-	sort.bsort(tab);
+	Sort sort=new Sort(tab);
+	sort.bsort();
 	for(int i=0; i<tab.length; i++)
 	{
 	System.out.print(tab[i]);
